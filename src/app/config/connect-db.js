09bee92c5a59f75df99ce72baa6d880d9014e7d5
Oseias Dev <oseiasbetodev@gmail.com>
2026-06-15 11:19:35 +0100

    Testando as cores [BETA]

--- src/app/config/connect-db.js
+++ src/app/config/connect-db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
     const appEnv = process.env.NODE_ENV
-    const StringConnection = appEnv == 'prod1' ? process.env.MONGO_ATLAS : appEnv == 'lan' ? process.env.MONGO_LAN : process.env.MONGO_lOCAL
+    const StringConnection = appEnv == 'prod' ? process.env.MONGO_ATLAS : appEnv == 'lan' ? process.env.MONGO_LAN : process.env.MONGO_lOCAL
     const connection = await mongoose.connect(StringConnection, {
         useNewUrlParser: true,
         useCreateIndex: true,
